33c7c5acc0c998f96037385f13f6e74379c54b96
Shreya Yadav <154625616+yadavshreya001@users.noreply.github.com>
2024-10-05 15:07:45 +0530

    Update powerLinear.java

--- src/Recursion/powerLinear.java
+++ src/Recursion/powerLinear.java
@@ -1,9 +1,14 @@
-package Recursion;
+// Question: Make a function which calculates 'a' raised to the power 'b' using recursion 
 
+package Recursion;
 import java.util.Scanner;
 
 public class powerLinear {
     public static int pow(int a, int b){
+        if(a == 0 && b == 0){  // edge case if base = 0 and power = 0
+            System.out.println("Not defined");
+            return -1;
+        }
         if(b == 0){
             return 1;
         }
