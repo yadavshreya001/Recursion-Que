4c6220fe4301019d68ac7565f7db05418459b729
Shreya Yadav <154625616+yadavshreya001@users.noreply.github.com>
2024-10-05 23:06:50 +0530

    Update NthStair.java

--- src/Recursion/NthStair.java
+++ src/Recursion/NthStair.java
@@ -6,9 +6,16 @@ import java.util.Scanner;
 
 public class NthStair {
     public static int stairPath(int n) {
-        if(n <= 2){ // base case
-            return n;
+       // Base cases
+        if (n == 0) {
+            return 1;  // Stay on the ground
         }
+        if (n == 1) {
+            return 1;  // One 1-step jump to reach stair 1
+        }
+        if (n == 2) {
+            return 2;  // Two ways to reach stair 2 (1+1 or 2)
+        }   
         return stairPath(n - 1) + stairPath(n - 2);
     }
     public static void main(String[] args) {
