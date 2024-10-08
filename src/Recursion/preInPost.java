efd7c7c3559918c0514002c608823fea48d51729
Shreya Yadav <154625616+yadavshreya001@users.noreply.github.com>
2024-10-09 01:04:47 +0530

    Pre-In-Post

---
+++ src/Recursion/preInPost.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class preInPost {
+    public static void pip(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.print(n + " ");
+        pip(n - 1);
+        System.out.print(n + " ");
+        pip(n - 1);
+        System.out.print(n + " ");
+    }
+    public static void main(String[] args) {
+        pip(3);
+    }
+}
